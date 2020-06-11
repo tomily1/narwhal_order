c7a28a09b0df37050fb4dd53bde22f19d028e5b5
Israel Tomilayo <itomilayo@gmail.com>
2020-06-11 17:37:30 +0100

    comment out initial views template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@
 
 class HomeController < AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @webhooks = ShopifyAPI::Webhook.find(:all)
+    @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
   end
 end
