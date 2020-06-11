20cb4bf1ae2512aacda0a2850538e2d788502374
Israel Tomilayo <itomilayo@gmail.com>
2020-06-11 16:14:12 +0100

    initialize dotenv in application config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to => 'orders#index'
+  root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
