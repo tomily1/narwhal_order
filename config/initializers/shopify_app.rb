06de48cef5c938151ad51ce665b4386ab282dfd2
Israel Tomilayo <itomilayo@gmail.com>
2020-06-11 16:02:59 +0100

    make changes to shopify scopes and suscribe to order webhooks

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,19 +1,18 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = Rails.application.credentials.shopify[:key]
-  config.secret = Rails.application.credentials.shopify[:secret]
+  config.api_key = ENV['SHOPIFY_API_KEY']
+  config.secret = ENV['SHOPIFY_API_SECRET']
   config.old_secret = ""
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "read_orders" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
   config.api_version = "2020-04"
   config.shop_session_repository = 'Shop'
   config.webhooks = [
-    {topic: 'app/uninstalled', address: "#{ENV['WEBHOOK_HOST']}/app_uninstalled", format: 'json'},
-    {topic: 'orders/delete', address: "#{ENV['WEBHOOK_HOST']}/orders_delete", format: 'json'},
-    {topic: 'orders/create', address: "#{ENV['WEBHOOK_HOST']}/orders_create", format: 'json'},
-    {topic: 'orders/updated', address: "#{ENV['WEBHOOK_HOST']}/orders_updated", format: 'json'}
+    {topic: 'orders/delete', address: "#{ENV['WEBHOOK_HOST']}/webhooks/orders_delete", format: 'json'},
+    {topic: 'orders/create', address: "#{ENV['WEBHOOK_HOST']}/webhooks/orders_create", format: 'json'},
+    {topic: 'orders/updated', address: "#{ENV['WEBHOOK_HOST']}/webhooks/orders_update", format: 'json'}
   ]
 end
 
